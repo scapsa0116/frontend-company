5282a0ce89b5d23b985b752d0894675080d1f6c9
github username <github email address>
2020-12-11 00:23:05 +0300

    styling the css

---
+++ photo_editing/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Slider from './Slider'
+import SidebarItem from './SidebarItem'
+
+
+const App = () => {
+    return(
+    <div className='container'>
+        <div className='main-image'/>
+           <div className='sidebar'>
+               <SidebarItem/> 
+               <SidebarItem/>
+               
+            </div> 
+        <Slider/>
+    </div>
+    )
+}
+
+
+export default App;
