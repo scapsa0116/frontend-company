0676836a8adfa3dd410881610eb8d281e5000403
github username <github email address>
2021-02-18 16:09:50 -0500

    working on edit and delete button

--- js-project/frontend/index.js
+++ js-project/frontend/index.js
@@ -1,31 +1,147 @@
 const url = "http://localhost:3000/drinks"
+
 document.addEventListener('DOMContentLoaded', () => {
     getFetch();
+
+
+    const createDrinksForm = document.querySelector("#create-drinks-form")
+
+    createDrinksForm.addEventListener("submit", (e) => 
+    createEventhandeler(e))
+
+    const drinkContainer = document.querySelector("#drinks-container")
+    drinkContainer.addEventListener("click", e => {
+        // console.log("clicked")
+        const id = parseInt(e.target.dataset.id);
+        const drink = Drink.findById(id);
+        // debugger;
+
+        console.log(drink)
+        // document.querySelector('#update-drink').innerHTML = drink.renderUpdateForm(e);
+
+        // document.querySelector('#update-drink').addEventListener('submit', e => updateFormHandler(e))
+
+    })
 })
 
+
+function updateFormHandler(e) {
+    e.preventDefault();
+    const id = parseInt(e.target.dataset.id);
+    const drink = Drink.findById(id);
+    const strDrink = document.querySelector("#input-strDrink").value
+    const strIngredient = document.querySelector("#input-strIngredient").value 
+    const strInstructions = document.querySelector("#input-strInstructions").value
+    const strDrinkThumb = document.querySelector("#input-strDrinkThumb").value
+    const category_id = parseInt(document.querySelector('#categories').value)
+    patchDrink(drink, strDrink, strIngredient, strInstructions,strDrinkThumb, category_id)
+  }
 
 function getFetch() {
     fetch(url) 
     .then(res => res.json())
     .then(drinks => {
         drinks.data.forEach(drink => {
-             
+        //    debugger;  
+       let newDrink = new Drink(drink, drink.attributes)
+            // const renderDrinks =`
+            // <div data-id =${drink.id}>
+            //   <img
+            //   src=${drink.attributes.strDrinkThumb}
+            //   height ="200" width="250">
+            //   <h3>${drink.attributes.strDrink}</h3>
+            //   <p>${drink.attributes.strIngredient}</p>
+            //   <p>${drink.attributes.strInstructions}</p>
+            //   <p>${drink.attributes.category.name}</p>
+            //   <button data-id=${drink.id}>Edit</button>
+            // </div>
+            // <br><br>`;
 
-            const renderDrinks =`
-            <div data-id =${drink.id}>
-              <img
-              src=${drink.attributes.strDrinkThumb}
-              height ="200" width="250">
-              <h3>${drink.attributes.strDrink}</h3>
-              <p>${drink.attributes.strIngredient}</p>
-              <p>${drink.attributes.strInstructions}</p>
-              <p>${drink.attributes.category.name}</p>
-              <button data-id=${drink.id}>Edit</button>
-            </div>
-            <br><br>`;
-
-            document.querySelector('#drinks-container').innerHTML += renderDrinks
+            document.querySelector('#drinks-container').innerHTML += 
+            newDrink.renderDrinkCard()
+            // debugger;
             
         })
+        // .catch(err => console.log(err))
     })
+}
+
+
+
+function createEventhandeler(e){
+e.preventDefault()
+
+const strDrink = document.querySelector("#input-strDrink").value
+const strIngredient = document.querySelector("#input-strIngredient").value 
+const strInstructions = document.querySelector("#input-strInstructions").value
+const strDrinkThumb = document.querySelector("#input-strDrinkThumb").value
+const categoryId = parseInt(document.querySelector('#categories').value)
+
+postfetch(strDrink, strIngredient, strInstructions,strDrinkThumb, categoryId)
+}
+
+function postfetch(strDrink, strIngredient, strInstructions,strDrinkThumb, category_id){
+    const bodyData = {strDrink, strIngredient, strInstructions,strDrinkThumb, category_id}
+ 
+    fetch(url, {
+     method: "POST",
+     headers: {"Content-Type": "application/json"},
+     body: JSON.stringify( bodyData
+        //  {
+        // strDrink:  strDrink,
+        // strIngredient: strIngredient,
+        // strInstructions: strInstructions,
+        // strDrinkThumb: strDrinkThumb,
+        // category_id: category_id}
+        )
+ })
+ 
+ .then(res => res.json())
+ .then(drink => {
+     console.log(drink);
+     const drinkData = drink.data
+    let newDrink = new Drink(drinkData, drinkData.attributes)
+    //  document.querySelector("#")
+
+
+    //  const drinksMarkup = 
+    //         `
+    //         <div data-id=${drink.id}>
+    //           <img
+    //           src=${drinkData.attributes.strDrinkThumb}
+    //           height ="200" width="250">
+    //           <h3>${drinkData.attributes.strDrink}</h3>
+    //           <p>${drinkData.attributes.strIngredient}</p>
+    //           <p>${drinkData.attributes.strInstructions}</p>
+    //           <p>${drinkData.attributes.category.name}</p>
+    //           <button data-id=${drinkData.id}>Edit</button>
+
+    //         </div>
+    //         `
+
+
+
+    //  let newDrink = new Drink(drinkData, drinkData.attributes)
+     document.querySelector("#drinks-container").innerHTML += 
+    //  drinksMarkup
+    newDrink.renderDrinkCard()
+ })
+
+
+ 
+}
+
+function deleteDrink(){
+    // event.preventDefault();
+    let drinkId = parseInt(event.target.dataset.id)
+
+    fetch(`${BASE_URL}/drinks/${drinkId}`,{
+        method: 'DELETE'
+
+         
+    })
+    
+    
+
+    this.location.reload();
 }
--- js-project/frontend/src/drink.js
+++ js-project/frontend/src/drink.js
@@ -1,29 +1,70 @@
 class Drink{
-    constructor(id, strDrink, strIngredient, strInstructions, category_id){
-        this.id = id;
-        this.strDrink = strDrink;
-        this.strIngredient = strIngredient;
-        this.strInstructions = strInstructions;
-        // this.strDrinkThumb = strDrinkThumb;
-        this.category_id = category_id
-        // this.category = category_id;
+    constructor(drink, drinkAttributes){
+        this.id = drink.id;
+        this.strDrink = drinkAttributes.strDrink;
+        this.strIngredient = drinkAttributes.strIngredient;
+        this.strInstructions = drinkAttributes.strInstructions;
+        this.strDrinkThumb = drinkAttributes.strDrinkThumb;
+        this.category = drinkAttributes.category;
+        Drink.all.push(this)
+        // debugger;
     }
 
-    //render drink instance method???
+    renderDrinkCard() {
+        // debugger;
+        return`
+        <div data-id =${this.id}>
+          <img
+          src=${this.strDrinkThumb}
+          height ="200" width="250">
+          <h3>${this.strDrink}</h3>
+          <p>${this.strIngredient}</p>
+          <p>${this.strInstructions}</p>
+          <p>${this.category.name}</p>
+          <button data-id=${this.id}>Edit</button>
+          <button class="delete-bttn" data-id=${this.id} onClick="deleteDrink()">Delete Drink</button>
+         
+        </div>
+        <br><br>`;
 
-    renderDrink(){
-        let drinksDiv = document.getElementById("drinks-container")
+        document.querySelector('#drinks-container').innerHTML += renderDrinks 
+    }
 
-        drinksDiv.innerHTML +=
-        `
-        <ul>
-        <br><br><h1>Cocktail Name: ${this.strDrink}</h1><br>
-        <li>Cocktail Ingredients: ${this.strIngredient}</li><br>
-        <li>Cocktail Instructions: ${this.strInstructions}</li><br>
-        <li>Cocktail category: ${this.category}</li><br>
-        </ul>
-        <button class="delete-bttn" data-id=${this.id} onClick="deleteDrink()">Delete Drink</button><br><br>
-        <button class="edite-bttn" data-id=${this.id} onClick="editeDrink()">Edite Drink</button><br><br>
-        `
+    static findById(id) {
+        return this.all.find(drink => drink.id === id)
+    }
+
+    renderUpdateForm(){
+        return`
+        <form data-id =${this.id}>
+        <h3>Edit a Drink!</h3>
+
+
+            <input id="input-strDrink" type="text" name="strDrink" value="${this.strDrink}" class="input-text">
+            <br><br>
+            <textarea id="input-strIngredient" name="strIngredient" rows="3" cols="30" value="${this.strIngredient}" ></textarea>
+            <br><br>
+            <textarea id="input-strInstructions" name="strInstructions" rows="3" cols="30" value="${this.strInstructions}" ></textarea>
+            <br><br>
+            <input id="input-strDrinkThumb" type="text" name="strDrinkThumb" value="${this.strDrinkThumb}"  class="input-text">
+            <br><br>
+
+            <p>Choose A category </p>
+            <select id="categories" name="categories" value="${this.category.name}">
+            <option value="9">Alcoholic</option>
+            <option value="10">Nonalcoholic</option>
+            </select>
+            <br><br>
+    
+          <input id='edit-button' type="submit" name="submit" value="Edit Drink" class="submit">
+        </form>
+        <br><br>`;
+    
+        
     }
 }
+
+
+Drink.all = [];
+
+
