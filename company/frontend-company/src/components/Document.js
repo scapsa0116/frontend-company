a19c13c114f6998cd38c9ef927deb26bc56e827a
github username <github email address>
2021-05-25 16:48:18 -0500

    working on double click

--- company/frontend-company/src/components/Document.js
+++ company/frontend-company/src/components/Document.js
@@ -1,65 +1,145 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 // import { Link } from "react-router-dom";
 
-function Document() {
+class Document extends React.Component {
   // const [toggle, setToggle] = React.useState(true);
-  const [documents, setDocuments] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [toggle, setToggle] = React.useState(true);
-  const [text, setText] = React.useState("Sriram");
+  // const [documents, setDocuments] = useState([]);
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [toggle, setToggle] = useState(true);
+  // const [text, setText] = React.useState("Sriram");
 
-  useEffect(() => {
-    fetch("http://localhost:3000/documents")
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/documents")
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setDocuments(result);
+  //       },
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     );
+  // }, []);
+
+  // useEffect(() => {
+  //   // POST request using fetch inside useEffect React hook
+  //   const requestOptions = {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ body: body })
+  //   };
+  //   fetch("http://localhost:3000/documents", requestOptions)
+  //     .then((response) => response.json())
+  //     .then((data) => setPostId(data.id));
+
+  //   // empty dependency array means this effect will only run once (like componentDidMount in classes)
+  // }, []);
+
+  state = {
+    documents: {},
+    toggle: true,
+    isLoaded: false,
+    id: "",
+    customer_code: "",
+    company_name: "",
+    cnpj: "",
+    address: "",
+    email: "",
+    editable: true
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/documents", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
       .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setDocuments(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
-
-  function toggleInput() {
-    setToggle(false);
+      .then((documents) => {
+        this.setState({
+          documents: documents,
+          isLoaded: true
+        });
+      });
   }
 
-  function handleChange(event) {
-    setDocuments(event.target.value);
+  toggleInput() {
+    // const documentId = this.props.match.params.documentId;
+    const documentId = document.id;
+    fetch(`http://localhost:3000/documents/${documentId}`, {
+      credentials: "include",
+      method: "PUT",
+      body: JSON.stringify({
+        document: document
+      }),
+
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        this.setState(
+          {
+            id: response.id,
+            customer_code: response.customer_code,
+            company_name: response.company_name,
+            cnpj: response.cnpj,
+            address: response.address,
+            email: response.email
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
   }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        {toggle ? (
-          <ul onDoubleClick={toggleInput}>
-            {documents.map((document) => (
-              <li key={document.id}>
-                {document.email} {document.adress}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <form>
-            {documents.map((document) => (
+  handleChange(event) {
+    this.setState({
+      documents: event.target.value
+    });
+  }
+
+  render() {
+    if (this.state.error) {
+      return <div>Error: {this.state.error.message}</div>;
+    } else if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {this.state.toggle ? (
+            <ul onDoubleClick={this.toggleInput}>
+              {this.state.documents.map((document) => (
+                <li key={document.id}>
+                  {document.email} {document.adress}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <form>
               <input
                 type='text'
                 value={document.email}
-                onChange={handleChange}
+                onChange={this.handleChange}
               />
-            ))}
-          </form>
-        )}
-      </div>
-    );
+              <input
+                type='text'
+                value={document.adress}
+                onChange={this.handleChange}
+              />
+            </form>
+          )}
+        </div>
+      );
+    }
   }
 }
 
