c30827a8743a7beffe46a4f8b5566534a03e5a8c
github username <github email address>
2021-05-28 15:19:36 -0500

    building edit

--- company/frontend-company/src/components/Edittext.js
+++ company/frontend-company/src/components/Edittext.js
@@ -9,6 +9,11 @@ class Edittext extends React.Component {
     cnpj: "",
     address: "",
     email: "",
+    distribuidor: "",
+    responsavel: "",
+    data: "",
+    visao: "",
+    missao: "",
     editable: true
   };
 
@@ -36,7 +41,12 @@ class Edittext extends React.Component {
             company_name: response.company_name,
             cnpj: response.cnpj,
             address: response.address,
-            email: response.email
+            email: response.email,
+            distribuidor: response.distribuidor,
+            responsavel: response.responsavel,
+            data: response.data,
+            visao: response.visao,
+            missao: response.missao
           },
           () => {
             console.log(this.state);
@@ -64,6 +74,11 @@ class Edittext extends React.Component {
     body.append("document[cnpj]", form.cnpj.value);
     body.append("document[address]", form.address.value);
     body.append("document[email]", form.email.value);
+    body.append("document[distribuidor]", form.distribuidor.value);
+    body.append("document[responsavel]", form.responsavel.value);
+    body.append("document[data]", form.data.value);
+    body.append("document[visao]", form.visao.value);
+    body.append("document[missao]", form.missao.value);
     const documentId = this.props.match.params.documentId;
 
     fetch(`http://localhost:3000/documents/${documentId}`, {
@@ -81,80 +96,109 @@ class Edittext extends React.Component {
 
   render() {
     return (
-      <div>
-        <form
-          onSubmit={this.handleSubmit}
-          className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'
-        >
+      <div onSubmit={this.handleSubmit}>
+        <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
           <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
-            2. Informações Gerais
+            1. Visao - Missao do Distribuidor
           </h1>
 
-          <fieldset className='mb-8 p-2 w-full  bg-grey-light'>
-            <h5 className='font-bold text-blue-600'>Código do Cliente: </h5>
-            <input
-              type='text'
-              onChange={this.handleInputChange.bind(this)}
-              value={this.state.customer_code}
-              ref='customer_code'
-              name='customer_code'
-              customer_code='customer_code'
-              id='customer_code'
-              className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
-            />
+          <h5 className='font-bold text-black-600'>visao:</h5>
+          <input
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.visao}
+            type='text'
+            ref='visao'
+            name='visao'
+            cnpj='visao'
+            id='visao'
+            // placeholder='visao'
+            className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+          />
 
-            <h5 className='font-bold text-blue-600'>Razão Social: </h5>
-            <input
-              onChange={this.handleInputChange.bind(this)}
-              value={this.state.company_name}
-              type='text'
-              ref='company_name'
-              name='company_name'
-              company_name='company_name'
-              id='company_name'
-              className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
-            />
+          <h5 className='font-bold text-black-600'>missao:</h5>
+          <input
+            onChange={this.handleInputChange.bind(this)}
+            value={this.state.missao}
+            type='text'
+            name='missao'
+            ref='missao'
+            company_name='missao'
+            id='missao'
+            // placeholder='missao'
+            className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+          />
 
-            <h5 className='font-bold text-blue-600'>CNPJ: </h5>
-            <input
-              onChange={this.handleInputChange.bind(this)}
-              value={this.state.cnpj}
-              type='text'
-              ref='cnpj'
-              name='cnpj'
-              cnpj='cnpj'
-              id='cnpj'
-              className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
-            />
+          <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
+            <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
+              2. Informações Gerais
+            </h1>
 
-            <h5 className='font-bold text-blue-600'>Endereço: </h5>
-            <input
-              onChange={this.handleInputChange.bind(this)}
-              value={this.state.address}
-              type='text'
-              name='address'
-              ref='address'
-              company_name='address'
-              id='address'
-              className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
-            />
+            <fieldset className='mb-8 p-2 w-full  bg-grey-light'>
+              <h5 className='font-bold text-blue-600'>Código do Cliente: </h5>
+              <input
+                type='text'
+                onChange={this.handleInputChange.bind(this)}
+                value={this.state.customer_code}
+                ref='customer_code'
+                name='customer_code'
+                customer_code='customer_code'
+                id='customer_code'
+                className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+              />
 
-            <h5 className='font-bold text-blue-600'>E-mail: </h5>
-            <input
-              onChange={this.handleInputChange.bind(this)}
-              value={this.state.email}
-              type='text'
-              ref='email'
-              name='email'
-              company_name='email'
-              id='email'
-              className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
-            />
-          </fieldset>
-          <button className=' p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200'>
-            Edit
-          </button>
-        </form>
+              <h5 className='font-bold text-blue-600'>Razão Social: </h5>
+              <input
+                onChange={this.handleInputChange.bind(this)}
+                value={this.state.company_name}
+                type='text'
+                ref='company_name'
+                name='company_name'
+                company_name='company_name'
+                id='company_name'
+                className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+              />
+
+              <h5 className='font-bold text-blue-600'>CNPJ: </h5>
+              <input
+                onChange={this.handleInputChange.bind(this)}
+                value={this.state.cnpj}
+                type='text'
+                ref='cnpj'
+                name='cnpj'
+                cnpj='cnpj'
+                id='cnpj'
+                className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+              />
+
+              <h5 className='font-bold text-blue-600'>Endereço: </h5>
+              <input
+                onChange={this.handleInputChange.bind(this)}
+                value={this.state.address}
+                type='text'
+                name='address'
+                ref='address'
+                company_name='address'
+                id='address'
+                className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+              />
+
+              <h5 className='font-bold text-blue-600'>E-mail: </h5>
+              <input
+                onChange={this.handleInputChange.bind(this)}
+                value={this.state.email}
+                type='text'
+                ref='email'
+                name='email'
+                company_name='email'
+                id='email'
+                className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'
+              />
+            </fieldset>
+            <button className=' p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition-all duration-200'>
+              Edit
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
--- company/frontend-company/src/components/TextComponent.js
+++ company/frontend-company/src/components/TextComponent.js
@@ -36,49 +36,49 @@ class TextComponent extends React.Component {
       <div>
         {this.state.documents.map((document, index) => (
           <div>
-            <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
-              <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
-                JBP 2021
-              </h1>
-              <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
-                Canal Distribuidores
-              </h1>
-
-              <fieldset className='mb-8 p-2 w-full  bg-grey-light'>
-                <h5 className='font-bold text-blue-600'>Distribuidor:</h5>
-                <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
-                  {document.distribuidor}
-                </div>
-
-                <h5 className='font-bold text-blue-600'>Responsavel:</h5>
-                <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
-                  {document.responsavel}
-                </div>
-
-                <h5 className='font-bold text-blue-600'>Data:</h5>
-                <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
-                  {document.data}
-                </div>
-              </fieldset>
-            </div>
-
-            <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
-              <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
-                1. Visao - Missao do Distribuidor
-              </h1>
-
-              <h5 className='font-bold text-black-600'>visao:</h5>
-              <div className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
-                {document.visao}
-              </div>
-
-              <h5 className='font-bold text-black-600'>missao:</h5>
-              <div className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
-                {document.missao}
-              </div>
-            </div>
-
             <Link to={`/documents/${document.id}`}>
+              <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
+                <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
+                  JBP 2021
+                </h1>
+                <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
+                  Canal Distribuidores
+                </h1>
+
+                <fieldset className='mb-8 p-2 w-full  bg-grey-light'>
+                  <h5 className='font-bold text-black-600'>Distribuidor:</h5>
+                  <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    {document.distribuidor}
+                  </div>
+
+                  <h5 className='font-bold text-black-600'>Responsavel:</h5>
+                  <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    {document.responsavel}
+                  </div>
+
+                  <h5 className='font-bold text-black-600'>Data:</h5>
+                  <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    {document.data}
+                  </div>
+                </fieldset>
+              </div>
+
+              <div className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
+                <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
+                  1. Visao - Missao do Distribuidor
+                </h1>
+
+                <h5 className='font-bold text-black-600'>visao:</h5>
+                <div className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                  {document.visao}
+                </div>
+
+                <h5 className='font-bold text-black-600'>missao:</h5>
+                <div className='h-20 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                  {document.missao}
+                </div>
+              </div>
+
               <div>
                 <form className='maw-w-6xl w-3/4 mx-auto mt-16 shadow-md px-4 py-6'>
                   <h1 className='mb-4 font-bold text-center text-blue-600 items-stretch  bg-grey-light'>
@@ -86,30 +86,30 @@ class TextComponent extends React.Component {
                   </h1>
 
                   <fieldset className='mb-8 p-2 w-full  bg-grey-light'>
-                    <h5 className='font-bold text-blue-600'>
+                    <h5 className='font-bold text-black-600'>
                       Código do Cliente:
                     </h5>
-                    <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
                       {document.customer_code}
                     </div>
 
-                    <h5 className='font-bold text-blue-600'>Razão Social:</h5>
-                    <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    <h5 className='font-bold text-black-600'>Razão Social:</h5>
+                    <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
                       {document.company_name}
                     </div>
 
-                    <h5 className='font-bold text-blue-600'>CNPJ:</h5>
-                    <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    <h5 className='font-bold text-black-600'>CNPJ:</h5>
+                    <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
                       {document.cnpj}
                     </div>
 
-                    <h5 className='font-bold text-blue-600'>Endereço:</h5>
-                    <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    <h5 className='font-bold text-black-600'>Endereço:</h5>
+                    <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
                       {document.address}
                     </div>
 
-                    <h5 className='font-bold text-blue-600'>E-mail:</h5>
-                    <div className='h-16 border-8 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
+                    <h5 className='font-bold text-black-600'>E-mail:</h5>
+                    <div className='h-16 border-2 w-full md:w-1/2 lg:w-1/4 bg-grey uppercase'>
                       {document.email}
                     </div>
                   </fieldset>
